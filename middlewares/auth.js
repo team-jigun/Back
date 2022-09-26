b871ebd5d5b1b10d8c2d85b5c749a9eb42db089c
dokdogalmaegi <clik202012@gmail.com>
2022-09-26 21:07:30 +0900

    refresh auth 구현

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,21 @@
 const jwt   = require('./jwt');
-const { TOKEN_EMPTY, TOKEN_EXPIRED, TOKEN_INVALID } = require('../modules/ERROR');
+const { TOKEN_OR_REFRESH_EMPTY, TOKEN_EXPIRED, TOKEN_INVALID } = require('../modules/ERROR');
 const util  = require('../modules/util');
 
 const authUtil = {
   checkToken: async (req, res, next) => {
-    let token = req.headers.authorization.replace('Bearer ', '');
-    const { refreshToken, id } = req.body;
+    const token = req.headers.authorization.replace('Bearer ', '');
+    const refreshToken = req.headers.refresh;
 
-    if (!token) {
-      return res.json(util.fail(TOKEN_EMPTY.code, TOKEN_EMPTY.message));
-    }
+    if (!token || !refreshToken) {
+      return res.json(util.fail(TOKEN_OR_REFRESH_EMPTY.code, TOKEN_OR_REFRESH_EMPTY.message));
+    } 
 
     const user = await jwt.verify(token);
+    const isNotExpiredRefresh = await jwt.refreshVerify(refreshToken, user.id);
     if (user === TOKEN_EXPIRED.code) {
       return res.json(util.fail(TOKEN_EXPIRED.code, TOKEN_EXPIRED.message));
-    } else if (user === TOKEN_INVALID.code || user.id === undefined) {
+    } else if (user === TOKEN_INVALID.code || !isNotExpiredRefresh || user.id === undefined) {
       return res.json(util.fail(TOKEN_INVALID.code, TOKEN_INVALID.message));
     }
 
--- routes/handler/userHandler.js
+++ routes/handler/userHandler.js
@@ -1,9 +1,44 @@
+const User = require('../../Schema/User');
+
+const { USER_ID, USER_INVALID, OTHER } = require('../../modules/ERROR');
 const util = require('../../modules/util');
 
 module.exports = {
-  test: async (req, res) => {
-    console.log(req);
+  changeUsername: async (req, res) => {
+    const { id, name } = req.body; 
 
-    res.json(util.success('test', 'test'));
+    try {
+      const user = await User.findOne({ id });
+
+      if (id !== req.id) {
+        console.log(req.id);
+        throw USER_INVALID;
+      }
+
+      if (user.name !== name) {
+        await User.updateOne({ id }, { $set: { name } });
+      }
+
+      return res.json(util.success('Success Change User Info', 'Success Change User Info'));
+    } catch (error) {
+      const { code } = error;
+
+      switch(code) {
+        case USER_ID.code:
+          return res.json(util.fail(USER_ID.code, USER_ID.message));
+        case USER_INVALID.code:
+          return res.json(util.fail(USER_INVALID.code, USER_INVALID.message));
+        default:
+          console.log(error);
+
+          if (error.name === 'TypeError') {
+            return res.json(util.fail(USER_INVALID.code, USER_INVALID.message));
+          }
+
+          console.log(error);
+          
+          return res.json(util.fail(OTHER.code, OTHER.message));
+      }
+    }
   }
 }
